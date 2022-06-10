a4a7665152457206b53c50a0d1a9cf839913e877
BruceCookie <329475282@qq.com>
2022-06-10 10:53:27 +0800

    deploy

---
+++ uploadbook.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+REPO_SSH='git@github.com:BruceCookie/BruceBook.git'  #[repository SSH]: copy from github repository page --> "code" button. 
+
+echo "executing：git add -A"
+git add -A
+
+echo "executing：commit -m 'deploy'"
+git commit -m 'update'
+
+echo "executing：git push -f $REPO_SSH master"
+git push -f $REPO_SSH master
+
+echo 'executing：gitbook build .'
+gitbook build .
+
+echo "executing：cd ./_book\n"
+cd ./_book
+
+echo "executing：git init\n"
+git init
+
+echo "executing：git add -A"
+git add -A
+
+echo "executing：commit -m 'deploy'"
+git commit -m 'deploy'
+
+echo "executing：git push -f $REPO_SSH master:gh-pages"
+git push -f $REPO_SSH master:gh-pages
+
+echo "back to upper directory"
+cd -
